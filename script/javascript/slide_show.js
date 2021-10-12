5d1af7ac13533b7f5b72858397b7cdddd035018b
Rui-i <j19981019@gmail.com>
2021-10-12 22:47:18 +0800

    banner_slideshow

---
+++ script/javascript/slide_show.js
@@ -0,0 +1,31 @@
+var slideIndex = 0;
+showSlides();
+banner_showSlides();
+
+function showSlides() {
+  var i;
+  var slides = document.getElementsByClassName("mySlides");
+  var dots = document.getElementsByClassName("dot");
+  for (i = 0; i < slides.length; i++) {
+    slides[i].style.display = "none";  
+  }
+  slideIndex++;
+  if (slideIndex > slides.length) {slideIndex = 1}    
+  for (i = 0; i < dots.length; i++) {
+    dots[i].className = dots[i].className.replace(" active", "");
+  }
+  slides[slideIndex-1].style.display = "block";  
+  dots[slideIndex-1].className += " active";
+  setTimeout(showSlides, 5000); // Change image every 2 seconds
+}
+function banner_showSlides() {
+    var i;
+    var slides = document.getElementsByClassName("myBanner");
+    for (i = 0; i < slides.length; i++) {
+      slides[i].style.display = "none";  
+    }
+    slideIndex++;
+    if (slideIndex > slides.length) {slideIndex = 1}    
+    slides[slideIndex-1].style.display = "block"; 
+    setTimeout(banner_showSlides, 3000); // Change image every 2 seconds
+  }
