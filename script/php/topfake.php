7c22d0055af4ea3b2f9aa5bccdc2b4e41fcc740d
elizabethhng <elizabethng30@gmail.com>
2021-10-14 12:40:04 +0800

    faketops

---
+++ script/php/topfake.php
@@ -0,0 +1,40 @@
+<?php 	$conn = mysqli_connect("localhost", "f32ee", "f32ee", "f32ee");
+
+	if (!$conn) {
+		die("Connection failed: " . mysqli_connect_error());
+	}
+	$sql = "SELECT * FROM f32ee.all_products WHERE product_type = 'tops'";
+	if ($result = mysqli_query($conn, $sql)) {
+
+        if (mysqli_num_rows($result) > 0) {
+            // output data of each row
+                $i=0;
+                while($row = mysqli_fetch_assoc($result)) {
+                    // echo "id: " . $row["id"]. " - Name: " . $row["firstname"]. " " . $row["lastname"]. "<br>";
+                if($i==0){echo "<tr>"; $i=3;}
+                        echo "<td><table>";
+                        echo "<img src=";
+                        echo $row["product_image"];
+                        echo "></td></tr>";
+                                        
+                        echo "<tr><th>";
+                        echo $row["product_name"];
+                        echo "</th></tr>";
+        
+                        echo "<tr><td style='padding: 1%;'>";
+                        echo "$";
+                        echo $row["product_price"];
+                        echo "</table></td>";
+                if($i==0){echo "</tr>"; }
+                $i--;
+                }
+
+            }
+
+	} else {
+		echo "Failed fetching data from database.";
+
+	mysqli_close($conn);
+    var_dump($row);
+}
+?>
