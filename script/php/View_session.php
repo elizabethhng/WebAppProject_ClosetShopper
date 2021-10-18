c0b57e7949f01a5fd6d023faeb7e20ab1a09534d
elizabethhng <elizabethng30@gmail.com>
2021-10-18 17:44:11 +0800

    ignore

--- script/php/View_session.php
+++ script/php/View_session.php
@@ -5,9 +5,9 @@ Session_start();
 $c=array_count_values($_SESSION['cart']);
 $ordered_items=serialize($c);
 
-// echo '<pre>';
-// var_dump($_SESSION);
-// echo $c[0];
+echo '<pre>';
+var_dump($_SESSION);
+echo $c[0];
 // echo '</pre>';
 // foreach ($c as $product_id => $qty) {
 //     // $arr[3] will be updated with each value from $arr...
@@ -15,16 +15,16 @@ $ordered_items=serialize($c);
 //    echo $qty."<br>";
 // }
 
-var_dump($c);
-echo '<br>serialise:  ;';
-var_dump($ordered_items);
+// var_dump($c);
+// echo '<br>serialise:  ;';
+// var_dump($ordered_items);
 
-echo '<br>';
-var_dump(unserialize($ordered_items));
+// echo '<br>';
+// var_dump(unserialize($ordered_items));
 
-foreach (unserialize($ordered_items) as $product_id => $qty) {
-    // $arr[3] will be updated with each value from $arr...
-   echo "<br>".$product_id.":";
-   echo $qty;
-}
+// foreach (unserialize($ordered_items) as $product_id => $qty) {
+//     // $arr[3] will be updated with each value from $arr...
+//    echo "<br>".$product_id.":";
+//    echo $qty;
+// }
 ?>
